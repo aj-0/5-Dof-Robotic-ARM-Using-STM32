3cc47a4c5e42187e9b53b0e1f8fbe21d24b78182
aj-0 <ajay360x@gmail.com>
2026-09-12 22:08:23 +0530

    git changes test3

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -62,7 +62,6 @@ static void MX_USART1_UART_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-void aj();
 
 uint8_t constrain_angle(uint8_t servo, uint8_t angle)
 {
