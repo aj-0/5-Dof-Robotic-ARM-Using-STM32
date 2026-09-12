dbb58b51d51e70b260e8beec6fb7d06618a7b429
aj-0 <ajay360x@gmail.com>
2026-09-12 21:57:21 +0530

    fgf

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -63,6 +63,8 @@ static void MX_USART1_UART_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 void aj();
+void j();
+
 uint8_t constrain_angle(uint8_t servo, uint8_t angle)
 {
     switch(servo)
